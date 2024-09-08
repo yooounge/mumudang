23cea39fb7a8c23fb8507666ab5f21c9d395e7e2
vdudeee_ <oon.n5757@gmail.com>
2024-09-08 23:07:07 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,3 +46,10 @@ function openBoard(evt, boardName) {
     evt.currentTarget.className += " opacity";
 }
 
+// board
+$(".answer").hide();
+$(".list").click(function(){
+    $(this).next().slideToggle()
+    $(this).children('.turn_wrap').children().toggleClass('turn')
+    $(this).children('.question').toggleClass('Q_color')
+});
